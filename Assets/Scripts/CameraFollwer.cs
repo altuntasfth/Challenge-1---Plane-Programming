5ad32b49fadc19d45dab4885edb8ec73f83206d1
Fatih Altuntas <fatihaltuntas44@gmail.com>
2020-05-03 14:00:49 +0300

    Proje eklendi.

---
+++ Assets/Scripts/CameraFollwer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollwer : MonoBehaviour
+{
+    public GameObject _player;
+    private Vector3 offset = new Vector3(30, 0, 10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject propellor;
+    public float _speed = 15.0f;
+    public float _rotationSpeed = 100f;
+    float x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        x += 900* Time.deltaTime;
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        transform.Rotate(Vector3.left * verticalInput * _rotationSpeed * Time.deltaTime);
+        propellor.transform.rotation = Quaternion.Euler(0, 0, x);
+    }
+}
